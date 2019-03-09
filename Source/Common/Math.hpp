f5634d6f8a6d275fc966b2cfc586e6d6dfa6cbb7
Piotr Doan <doanpiotr@gmail.com>
2019-03-09 18:06:48 +0100

    Add additional math function for degrees/radians conversion and vector dot/cross products

--- Source/Common/Math.hpp
+++ Source/Common/Math.hpp
@@ -1,7 +1,29 @@
 #pragma once
 
+const float Pi = 3.1415926535897932384626433f;
+
+inline float dotProduct(const sf::Vector2f& a, const sf::Vector2f b)
+{
+    return a.x * b.x + a.y * b.y;
+}
+
+inline float crossProduct(const sf::Vector2f& a, const sf::Vector2f b)
+{
+    return a.x * b.y - a.y * b.x;
+}
+
+inline float toRadians(const float& degrees)
+{
+    return (degrees * Pi) / 180.0f;
+}
+
+inline float toDegrees(const float& radians)
+{
+    return (radians * 180.0f) / Pi;
+}
+
 template<typename Type>
-Type Lerp(const Type& from, const Type& to, float alpha)
+Type lerp(const Type& from, const Type& to, float alpha)
 {
     return (1.0f - alpha) * from + alpha * to;
 }
